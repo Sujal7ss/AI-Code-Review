9044712677964464e112a37a15cc707108ae1d98
Sujal7ss <sujalchahande3@gmail.com>
2025-03-15 17:24:14 +0530

    Style and implemented frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+// import "prismjs/components/prism-jsx";
+// import "prismjs/components/prism-python"
+import prism from "prismjs";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
+import Markdown from "react-markdown";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum() {
+  return 1 + 1;
+}
+  `);
 
+  const [response, setResponse] = useState("");
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    const response = await axios.post("http://localhost:3000/ai/get-review", { code })
+    setResponse(response.data)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                width: "100%",
+                height: "100%",
+                overflow: "auto",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                backgroundColor: "black",
+              }}
+            ></Editor>
+          </div>
+          <div onClick={reviewCode} className="review_button">
+            Review
+          </div>
+        </div>
+        <div className="right">
+          <Markdown>{response}</Markdown>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
