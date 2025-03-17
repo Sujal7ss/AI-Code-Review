15bf13f81e0049c64732614350b20606756d94fd
Sujal7ss <sujalchahande3@gmail.com>
2025-03-17 14:54:35 +0530

    Fixing frontend url for cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173", "https://ai-code-review-pink.vercel.app/"
+  origin: ["http://localhost:5173", "https://ai-code-review-pink.vercel.app"]
 };
 app.use(cors(corsOptions));
 app.use(express.json());
